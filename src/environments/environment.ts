067b1381183ca8f0c080fa5c2c01738a7b477eb9
David Bejar Caceres <davidbejar2009@gmail.com>
2019-05-30 01:16:50 +0200

    Esconde las claves privadas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCYeSfgPXPaxhea1sddPDBb60pb_fAV1fc",
-    authDomain: "hibrido-ee783.firebaseapp.com",
-    databaseURL: "https://hibrido-ee783.firebaseio.com",
-    projectId: "hibrido-ee783",
-    storageBucket: "hibrido-ee783.appspot.com",
-    messagingSenderId: "237907313995",
-    appId: "1:237907313995:web:a503b83003d99b02"
+    apiKey: "REPLACE",
+    authDomain: "REPLACE",
+    databaseURL: "REPLACE",
+    projectId: "REPLACE",
+    storageBucket: "REPLACE",
+    messagingSenderId: "REPLACE",
+    appId: "REPLACE"
   }
 };
 
