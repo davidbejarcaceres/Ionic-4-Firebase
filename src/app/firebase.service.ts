0afc3b6f75bd16b62c61edefa68bc6780d1d19a1
David Bejar Caceres <davidbejar2009@gmail.com>
2019-06-15 21:18:51 +0200

    Activates offline firebase

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -12,7 +12,35 @@ import { ToastController } from '@ionic/angular';
 export class FirebaseService {
 
   constructor(private firestore: AngularFirestore, private toastController: ToastController) {
-
+    firestore.firestore.enablePersistence()
+    .catch(function(err) {
+      if (err.code == 'failed-precondition') {
+          // Multiple tabs open, persistence can only be enabled
+          // in one tab at a a time.
+          // ...
+          var errorType = "Multiple tabs open, persistence can only be enabled in one tab at a a time.";
+          console.log(errorType);
+          const toast =  this.toastController.create({
+            message: (errorType),
+            duration: 1000,
+            position: 'bottom',
+          });
+          toast.present();
+          
+      } else if (err.code == 'unimplemented') {
+          // The current browser does not support all of the
+          // features required to enable persistence
+          // ...
+          var errorType = "The current browser does not support all of the features required to enable persistence";
+          console.log(errorType);
+          const toast =  this.toastController.create({
+            message: (errorType),
+            duration: 1000,
+            position: 'bottom',
+          });
+          toast.present();
+      }
+    });
   }
 
 
